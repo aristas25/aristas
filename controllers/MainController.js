6e10b0a825edde10423ae4feb9a421093f226740
aristas25 <apparistas@gmail.com>
2025-04-09 17:54:28 -0300

    Update MainController.js

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -12,4 +12,19 @@ self.getMain = async (req, res) => {
   }
 };
 
+
+self.getMore = async (req, res) => {
+  try {
+    const { data, error } = await supabase
+      .from("users") // nombre de la tabla
+      .select("*")      // columnas a seleccionar
+
+    if (error) throw error;
+
+    res.json({ status: "ok", data });
+  } catch (e) {
+    res.json({ error: e.message });
+  }
+};
+
 module.exports = self;
