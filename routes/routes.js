77bf3ffa96adf92fef06bc11ed26bf0d3d9fa881
aristas25 <apparistas@gmail.com>
2025-04-09 17:54:49 -0300

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,6 @@ const MainController = require("../controllers/MainController");
 // USER
 router.get("/main", (req, res, next) => MainController.getMain(req, res, next));
 
+router.get("/more", (req, res, next) => MainController.getMore(req, res, next));
+
 module.exports = router;
